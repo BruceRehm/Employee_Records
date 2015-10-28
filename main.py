46e02eb83103053475f1aa392c31ad6024dc3eb6
BruceRehm <brucerehm@gmail.com>
2015-10-28 18:45:00 -0400

    Final Draft Employee Records

--- main.py
+++ main.py
@@ -10,51 +10,92 @@ quit or exit
 
 """
 read from console
+
+bonus is read from a comma delimited file.
+also do the final note for dict
 """
+#Define Dictionary for employees and ages
+employee_db = {"Rob" : 39, "Amit" : 33, "Bruce" : 50, "Fred" : 60}
 
-employee_db = {"Rob" : 39, "Amit" : 33, "Bruce" : 50 }
-print employee_db["Rob"]
+#Set up Show All Employees Function
 
-def show_all():
-    print "show_all"
+def show_all(dictionary):
+   for key, value in dictionary.items():
+       print (key, value)
 
-def add_employee(name, age):
-    print "add_employee()"
+#Set up Add Employee Function
 
-def delete_employee():
-    print "delete_employee"
+def add_employee(dictionary):
+    employee_db.update({'Phil':55})
+    selection = raw_input("Enter New Employee Name: ")
+    kinput = selection
+    print kinput
+    selection = raw_input("Enter New Employee Age: ")
+    vinput = selection
+    print vinput
+    #return selection
+    employee_db.update({kinput: vinput})
 
-def find_employee():
-    print "find_employee"
+#Set up Delete Employee Function
+
+def delete_employee(dictionary):
+    selection = raw_input("Enter Employee you wish to TERMINATE!: ")
+    # return selection
+    einput = selection
+    selection = raw_input("Are you sure you want to delete? ")
+    dinput = selection
+    print dinput
+    if dinput == "Y":
+        del employee_db[einput]
+
+    elif dinput != "Y":
+        print "Thanks Anyways"
+
+
+#Set up Find Employee Function
+
+def find_employee(dictionary):
+    selection = raw_input("Enter Employee you wish to Creep on!: ")
+    # return selection
+    einput = selection
+    print einput
+    for key, value in dictionary.items():
+        if key == einput:
+            print (key, value)
+
+#Set up Exit Function
 
 def exit():
     print "exit"
 
-print "Welcome to Employee Database System"
+#Main Program
 
 while True:
     def menu_options():
-         print "1. Show All Employees" + "\n" + "2. Add new Employee" + "\n" + "3. Delete Existing Employee" + "\n" + "4. FInd Employees" + "\n" + "5. Exit Employee Database"
-         selection = raw_input("Enter a number 1 -5")
+         print "\n"
+         print "Welcome to the Employee Database System"
+         print "1. Show All Employees" + "\n" + "2. Add New Employee" + "\n" + "3. Delete Existing Employee" + "\n" + "4. Find Employees" + "\n" + "5. Exit Employee Database " + "\n"
+         selection = raw_input("Enter a number 1 - 5 ")
          return selection
 
     user_selection = int(menu_options())
-
-    print user_selection
+    print "\n"
+    print "Your Selection is: ", user_selection
+    print "\n"
 
     if user_selection == 1:
-        show_all()
+        show_all(employee_db)
 
     elif user_selection == 2:
-        add_employee()
+        add_employee(employee_db)
 
     elif user_selection == 3:
-        delete_employee()
+        delete_employee(employee_db)
 
     elif user_selection == 4:
-        find_employee()
+        find_employee(employee_db)
 
     elif user_selection == 5:
-        print "Later"
+        print "Thank you for using the Employee Database System"
         break
 
